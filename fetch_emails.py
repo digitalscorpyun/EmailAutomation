852ec3db07b39ca63595f0545fffb721f9bbe2ec
miker <mikekibbe73@gmail.com>
2025-01-31 00:20:43 -0800

    Fixed indentation error in fetch_emails.py

--- fetch_emails.py
+++ fetch_emails.py
@@ -27,13 +27,8 @@ def authenticate_gmail():
         creds = Credentials.from_authorized_user_file(TOKEN_FILE)
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-        creds = flow = InstalledAppFlow.from_client_secrets_file(
-    "credentials.json",
-    scopes=["https://www.googleapis.com/auth/gmail.readonly"],
-)
-creds = flow.run_local_server(port=0, access_type="offline", prompt="consent")  # Force refresh token
-port=0
-    with open(TOKEN_FILE, "w") as token:
+        creds = flow.run_local_server(port=0)
+        with open(TOKEN_FILE, "w") as token:
             token.write(creds.to_json())
     return creds
 
